48ffe2f411e21e01fb077c638c32e05e0a68dad5
Haoye <haoye999han@gmail.com>
2021-06-23 23:31:10 +0800

    chore: 重构代码

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ const configUser = require('./config');
 
 const configuration = {
   cantBuy: '配置中',
-  canBuy: '赶紧抢'
+  canBuy: '赶紧抢',
 };
 
-function getInfo(id) {
+async function getInfo(id) {
   const url = `http://www.ziroom.com/x/${id}.html`;
 
-  return axios({
+  const res = await axios({
     method: 'GET',
     url,
     // headers: {
     //   Cookie: configUser.Cookie || ''
     // }
-  })
-    .then(res => cheerio.load(res.data))
-    .then($ => $('.Z_info_aside .Z_name .status').hasClass('iconicon_release'));
+  });
+
+  const $ = cheerio.load(res.data);
+  return $;
 }
 
 async function sendMail(msg) {
@@ -31,32 +32,42 @@ async function sendMail(msg) {
     secure: true,
     auth: {
       user: configUser.user,
-      pass: configUser.pass
-    }
+      pass: configUser.pass,
+    },
   });
 
   transporter.sendMail({
     from: `"自如买啊买" ${configUser.user}`,
     to: configUser.to,
     subject: '[！！！重要！！！]赶紧买',
-    text: msg
+    text: msg,
   });
+}
+
+async function handleID(id, timer) {
+
+  const $ = await getInfo(id);
+
+  const ing = $('.Z_info_aside .Z_name .status').hasClass(
+    'iconicon_release'
+  );
+  if (!ing) {
+    console.log(configuration.canBuy);
+    sendMail(`
+      买了买了
+      ${$('.Z_info_aside .Z_name').html()}
+      ${$('.Z_home_b').html()}
+    `);
+    clearInterval(timer);
+  } else {
+    console.log(configuration.cantBuy, '   ', new Date().toLocaleString());
+  }
 }
 
 function main() {
   const timer = setInterval(() => {
-    configUser.ids.forEach((id) => {
-      getInfo(id).then(ing => {
-        if (!ing) {
-          console.log(configuration.canBuy);
-          sendMail('买了买了');
-          clearInterval(timer);
-        } else {
-          console.log(configuration.cantBuy, '   ', (new Date()).toLocaleString());
-        }
-      });
-    });
-  }, 10000);
+    configUser.ids.forEach(id => handleID(id, timer));
+  }, 30000);
 }
 
 main();
